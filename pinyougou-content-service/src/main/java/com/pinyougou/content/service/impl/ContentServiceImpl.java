0ab9130d6f99a4567dfd4eba354014f14f2a4179
yiwei <1321819328@qq.com>
2019-08-30 00:22:42 +0800

    day08

--- pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -7,6 +7,7 @@ import com.pinyougou.content.service.ContentService;
 import com.pinyougou.mapper.ContentMapper;
 import com.pinyougou.model.Content;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
@@ -16,6 +17,9 @@ public class ContentServiceImpl implements ContentService {
 
     @Autowired
     private ContentMapper contentMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
 	/**
 	 * 返回Content全部列表
@@ -53,7 +57,11 @@ public class ContentServiceImpl implements ContentService {
      */
     @Override
     public int add(Content content) {
-        return contentMapper.insertSelective(content);
+        int count = contentMapper.insertSelective(content);
+        if (count > 0) {
+            redisTemplate.boundHashOps("content").delete(content.getCategoryId());
+        }
+        return count;
     }
 
 
@@ -75,7 +83,15 @@ public class ContentServiceImpl implements ContentService {
      */
     @Override
     public int updateContentById(Content content) {
-        return contentMapper.updateByPrimaryKeySelective(content);
+        Content oldContent = contentMapper.selectByPrimaryKey(content.getId());
+        int count = contentMapper.updateByPrimaryKeySelective(content);
+        if (count > 0) {
+            redisTemplate.boundHashOps("content").delete(oldContent.getCategoryId());
+            if (content.getCategoryId().longValue() != oldContent.getCategoryId().longValue()) {
+                redisTemplate.boundHashOps("content").delete(content.getCategoryId());
+            }
+        }
+        return count;
     }
 
 
@@ -86,22 +102,45 @@ public class ContentServiceImpl implements ContentService {
      */
     @Override
     public int deleteByIds(List<Long> ids) {
+        //查询出所有分类
+        Example example1 = new Example(Content.class);
+        Example.Criteria criteria1 = example1.createCriteria();
+        criteria1.andIn("id",ids);
+        List<Content> contents = contentMapper.selectByExample(example1);
+
         //创建Example，来构建根据ID删除数据
         Example example = new Example(Content.class);
         Example.Criteria criteria = example.createCriteria();
 
         //所需的SQL语句类似 delete from tb_content where id in(1,2,5,6)
         criteria.andIn("id",ids);
-        return contentMapper.deleteByExample(example);
+        int count = contentMapper.deleteByExample(example);
+
+        //删除对应缓存
+        if (count > 0) {
+            for (Content content : contents) {
+                redisTemplate.boundHashOps("content").delete(content.getCategoryId());
+            }
+        }
+        return count;
     }
 
     @Override
     public List<Content> findByCategoryId(long categoryId) {
+        List<Content> contents = (List<Content>)redisTemplate.boundHashOps("content").get(categoryId);
+        if (contents != null) {
+            return contents;
+        }
         Example example = new Example(Content.class);
         Example.Criteria criteria = example.createCriteria();
         criteria.andEqualTo("categoryId",categoryId);
         criteria.andEqualTo("status",Content.CONTENT_STATUS_START);
         example.orderBy("sortOrder").asc();
-        return contentMapper.selectByExample(example);
+        contents = contentMapper.selectByExample(example);
+        if (contents != null && contents.size() > 0) {
+            //存入缓存
+            redisTemplate.boundHashOps("content").put(categoryId,contents);
+        }
+        return contents;
     }
 }
